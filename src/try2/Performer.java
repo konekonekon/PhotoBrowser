5afbc1ae3c6d3396419f3ebb8a956a97773bebc3
Konekonekon <ayanotarumi@gmail.com>
2016-10-17 09:18:40 +0200

    Add new classes.

---
+++ src/try2/Performer.java
@@ -0,0 +1,12 @@
+package try2;
+
+/*** For Window ***/
+public interface Performer {
+
+	public void importFile();
+	public void delete();
+	public void quit();
+	public void photoViewer();
+	public void browser();
+	public void split();
+}
